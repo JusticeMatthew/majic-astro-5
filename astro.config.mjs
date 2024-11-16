b7ecb9114be7171bd963fb54a2c2cffb9a0635c9
Matthew <matthewajustice@gmail.com>
2024-11-16 03:49:29 -0500

    🚀 Adds source maps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://majicwebdesign.netlify.app',
-  integrations: [tailwind(), icon()]
+  site: "http://majicwebdesign.netlify.app",
+  integrations: [tailwind(), icon()],
+  vite: {
+    build: {
+      sourcemap: true,
+    },
+  },
 });
