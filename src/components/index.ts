c47648ee02dfaf96c48ee8340f97c6c70d82ae44
Matthew <matthewajustice@gmail.com>
2024-11-16 20:23:37 -0500

    ✨ Adds stars! and fixes some cross browser styles, examples section started

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export { default as Button } from "./atoms/Button.astro";
 export { default as Benefits } from "./atoms/Benefits.astro";
+export { default as Stars } from "./atoms/Stars.astro";
 export { default as Landing } from "./sections/Landing.astro";
 export { default as About } from "./sections/About.astro";
 export { default as Services } from "./sections/Services.astro";
 export { default as Packages } from "./sections/Packages.astro";
+export { default as Examples } from "./sections/Examples.astro";
