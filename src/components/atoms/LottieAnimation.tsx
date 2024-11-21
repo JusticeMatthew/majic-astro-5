c29592966ea3567ea73d80f7392b84a1e579b68f
Matthew <matthewajustice@gmail.com>
2024-11-21 18:44:18 -0500

    💫 Testing solidjs lottie animation component

---
+++ src/components/atoms/LottieAnimation.tsx
@@ -0,0 +1,27 @@
+import Lottie from "lottie-web";
+import { createEffect } from "solid-js";
+
+interface LottieAnimationProps {
+  selector: string;
+  styles: string;
+}
+
+export default function LottieAnimation({
+  selector,
+  styles,
+}: LottieAnimationProps) {
+  createEffect(() => {
+    const lottieContainer = document.getElementById(
+      `lottie-container:${selector}`,
+    ) as HTMLElement;
+
+    const animation = Lottie.loadAnimation({
+      container: lottieContainer,
+      renderer: "canvas",
+      loop: true,
+      autoplay: true,
+      path: `/animations/${selector}.json`,
+    });
+  });
+  return <figure id={`lottie-container:${selector}`} class={styles}></figure>;
+}
