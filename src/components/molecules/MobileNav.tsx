100e9df6a9582b25a46fed3c92226f838144012a
Matthew <matthewajustice@gmail.com>
2025-04-14 15:53:10 -0400

    more mobile nav tweaks

--- src/components/molecules/MobileNav.tsx
+++ src/components/molecules/MobileNav.tsx
@@ -1,19 +1,26 @@
 import navLinks from "$/constants/navLinks.ts";
-import { children as resolveChildren, createSignal } from "solid-js";
+import { children as resolveChildren, createSignal, onMount } from "solid-js";
 
 interface MobileNavProps {
   children: HTMLElement[];
 }
 
 export default function MobileNav(props: MobileNavProps) {
+  const [icons, setIcons] = createSignal({
+    menuIcon: <></>,
+    closeIcon: <></>,
+    button: <></>,
+  });
   const resolved = resolveChildren(() => props.children);
   const childElement = resolved() as HTMLElement;
 
-  const astro = {
-    menuIcon: childElement.children[0],
-    closeIcon: childElement.children[1],
-    button: childElement.children[2],
-  };
+  onMount(() => {
+    setIcons({
+      menuIcon: childElement.children[0],
+      closeIcon: childElement.children[1],
+      button: childElement.children[2],
+    });
+  });
 
   const [menuOpen, setMenuOpen] = createSignal(false);
 
@@ -27,26 +34,26 @@ export default function MobileNav(props: MobileNavProps) {
         onClick={handleMenuToggle}
         aria-label="nav menu"
         class="p-3 active:scale-95">
-        {astro.menuIcon}
+        {icons().menuIcon}
       </button>
       <aside
-        class={`${menuOpen() ? "-translate-x-[66%]" : "translate-x-full"} fixed top-0 h-screen w-fit drop-shadow-lg transition-all duration-300 will-change-transform`}>
-        <div class="from-dark-bg flex h-full w-full justify-end bg-gradient-to-l to-transparent backdrop-blur">
-          <nav class="bg-light-purple flex h-full min-w-52 flex-col">
-            <button onClick={handleMenuToggle}>{astro.closeIcon}</button>
-            <div class="text-dark mt-6 flex h-full w-full flex-col justify-between px-4">
+        class={`${menuOpen() ? "-translate-x-[60%]" : "translate-x-[120%]"} fixed top-0 h-screen min-w-80 drop-shadow-lg transition-all duration-300 will-change-transform`}>
+        <div class="from-dark-bg flex h-full w-[90%] justify-end bg-gradient-to-l to-transparent backdrop-blur">
+          <nav class="bg-light-purple flex h-full min-w-80 flex-col">
+            <button onClick={handleMenuToggle}>{icons().closeIcon}</button>
+            <div class="text-dark mt-6 flex h-full w-full flex-col justify-between">
               <div class="flex w-full flex-col items-center">
                 {navLinks.map(({ href, label }) => (
                   <a
                     href={href}
                     onClick={handleMenuToggle}
-                    class="text-level-2 hover:bg-blurple/30 focus:bg-blurple/30 w-full rounded-lg py-3 text-center active:scale-95 [&:nth-child(3)]:mb-3">
+                    class="text-level-2 hover:bg-blurple/30 focus:bg-blurple/30 max-w-90 rounded-lg px-4 py-3 text-center active:scale-95 [&:nth-child(3)]:mb-3">
                     {label}
                   </a>
                 ))}
               </div>
               <div class="mb-16 flex w-full flex-col items-center">
-                {astro.button}
+                {icons().button}
                 <p class="mt-2 mb-1">or call:</p>
                 <a href="tel:3866019344">386-601-9344</a>
               </div>
